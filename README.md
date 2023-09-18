99721e7442ed719c26326084a7a1dcaf7dbfe18a
Carlos Moreno <cfmorenod@gmail.com>
2023-09-17 22:52:33 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# simple_image_resizer
-A python code developed for resizing and organizing files
+<h1>What is it?</h1>
+
+A simple python script developed for resizing and organizing files
+
+<h1>How to use it</h1>
+
+0. Clone this repository
+
+1. On terminal, run the following commands: "pip install shutils" and "pip install Pillow" if not already installed
+
+2. Drag and drop your images in the "images" folder on the root of the repository (Create this folder if it does not exists)
+
+3. Run main.py from the root folder
+
+4. The resized images will be available in the "resized" folder
---
+++ main.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os
+import shutil
+
+# Set images folder
+folder_path = "images"
+
+# Loop through the files in the folder
+for file in os.listdir(folder_path):
+    # Check if the file is an image
+    if file.endswith(".jpg") or file.endswith(".png"):
+        # Open the image file
+        img = Image.open(os.path.join(folder_path, file))
+        # Resize the image to 512 x 512 pixels
+        img = img.resize((512, 512))
+        # Save the resized image as a new file
+        img.save(os.path.join(folder_path, "resized_" + file))
+
+# Set Resized images folder        
+resized_path = "resized"
+
+# For the resized images, move them to the resized folder:
+
+for file in os.listdir(folder_path):
+    # Check if the file starts with resized_
+    if file.startswith("resized_"):
+        # Copy the file to the resized folder
+        shutil.copy(os.path.join(folder_path, file), resized_path)
+        # Delete the file from the original folder
+        os.remove(os.path.join(folder_path, file)) 
